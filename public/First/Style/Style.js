6367dc73ae03c68aa83713b82dde4580b682b757
Gleb952 <lomakin.plex@mail.ru>
2022-11-18 21:43:25 +0300

    htmlway

---
+++ public/First/Style/Style.js
@@ -0,0 +1,31 @@
+var i = 0;
+
+function move() {
+    if (i == 0) {
+        i = 1;
+        var elem = document.getElementById("myBar");
+        var width = 1;
+        var id = setInterval(frame, 2100);
+
+        function frame() {
+            if (width >= 100) {
+                clearInterval(id);
+                i = 0;
+            } else {
+                width++;
+                elem.style.width = width + "%";
+            }
+        }
+    }
+}
+
+document.addEventListener("DOMContentLoaded", move);
+
+
+
+
+
+
+setTimeout(function () {
+    window.location.href = "../Second/T2.html";
+}, 210000);v
